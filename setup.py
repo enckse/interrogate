f4d621cda8c1f16fb110ed10d45875066cfb9fcc
Sean Enck <enckse@gmail.com>
2017-06-20 12:19:02 -0400

    gitignore

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='survey',
-    version="0.1.0",
-    description="a basic/primitive survey app for LAN-based surveys",
-    url='https://github.com/epiphyte/survey',
-    license='MIT'
-)
