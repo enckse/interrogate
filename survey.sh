2d06e66cb2f7de66b6187de9d88fa72b282ebf4b
Sean Enck <enckse@gmail.com>
2017-10-27 15:58:53 -0400

    env file for survey

--- survey.sh
+++ survey.sh
@@ -3,6 +3,10 @@ cd /opt/epiphyte/survey/
 if [ -e env ]; then
     source env
 fi
+args="$@"
+if [ -z "$args" ]; then
+    args=$SURVEY_SETTINGS
+fi
 exit_code=1
 while [ $exit_code -ne 0 ]; do
     echo "loading survey $@"
