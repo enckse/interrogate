22fbc36f918f7e250adffec66f7068b276f97d05
Sean Enck <enckse@gmail.com>
2017-10-30 13:57:14 -0400

    whitespace

--- util/find_completed.sh
+++ util/find_completed.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 _proc() {
 for u in $(find $1 -type f | cut -d "/" -f 3 | sort | uniq); do
-	match=1
-	for f in $(find $1 -type f | grep "$u" | grep "save"); do
-		echo $f
-		match=0
-	done
-	if [ $match -eq 1 ]; then
-		for f in $(find $1 -type f | grep "$u" | sort -r); do
-			echo $f
-			break
-		done
-	fi
+        match=1
+        for f in $(find $1 -type f | grep "$u" | grep "save"); do
+                echo $f
+                match=0
+        done
+        if [ $match -eq 1 ]; then
+                for f in $(find $1 -type f | grep "$u" | sort -r); do
+                        echo $f
+                        break
+                done
+        fi
 done
 }
 
 if [ -z "$1" ]; then
-	echo "path required"
-	exit 1
+        echo "path required"
+        exit 1
 fi
 
 _proc "$1"
