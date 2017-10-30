bab925cabc926fd3eda8075f8d82b38ae5e8a2a7
Sean Enck <enckse@gmail.com>
2017-10-30 13:55:57 -0400

    updates to stitching and html table output

--- util/stitcher.py
+++ util/stitcher.py
@@ -5,12 +5,20 @@ import json
 import csv
 import sys
 
-def get_all(input_path):
+def get_all(input_path, include):
     """get all json objects"""
     for root, dirs, files in os.walk(input_path):
         # detect files here
         for item in files:
             path = root + "/" + item
+            if len(include) > 0:
+                do_continue = True
+                for inc in include:
+                    if inc in path:
+                        do_continue = False
+                        break
+                if do_continue:
+                    continue
             with open(path, 'r') as f:
                 j = json.loads(f.read())
                 j['z-meta-file'] = path
@@ -20,12 +28,12 @@ def get_all(input_path):
                     idx = idx + 1
                 yield j
 
-def stitch(path, json_out, csv_out):
+def stitch(path, json_out, csv_out, include):
     """Stitch together."""
     json_out.write("[");
     first = True
     keys = []
-    for obj in get_all(path):
+    for obj in get_all(path, include):
         if not first:
             json_out.write(',')
         first = False
@@ -42,14 +50,17 @@ def stitch(path, json_out, csv_out):
                             fieldnames=keys,
                             quoting=csv.QUOTE_NONNUMERIC)
     writer.writeheader()
-    for obj in get_all(path):
+    for obj in get_all(path, include):
         writer.writerow(obj)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print('requires an input dir and output file')
         exit(-1)
+    including = []
+    if len(sys.argv) > 3:
+        including = sys.argv[3:]
     output_name = sys.argv[2]
     with open(output_name + '.json', 'w') as json_file:
         with open(output_name + '.csv', 'w') as csv_file:
-            stitch(sys.argv[1], json_file, csv_file)
+            stitch(sys.argv[1], json_file, csv_file, including)
--- util/to_table.py
+++ util/to_table.py
@@ -9,18 +9,22 @@ def main():
         print("requires input file (csv)")
         exit(1)
     print("<table>")
+    skip_idx = []
     with open(sys.argv[1]) as f:
         reader = csv.reader(f)
         for r in reader:
+            idx = 0
             print("<tr>")
             for item in r:
-                print("<td>")
-                print(escape(item))
-                print("</td>")
+                if item.startswith("z-meta"):
+                    skip_idx.append(idx)
+                if idx not in skip_idx:
+                    print("<td>")
+                    print(escape(item))
+                    print("</td>")
+                idx += 1
             print("</tr>")
     print("</table>")
 
 if __name__ == "__main__":
     main()
-
-
