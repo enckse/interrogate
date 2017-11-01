b03cc26116693792801b4620b2fa654e7ea8c62d
Sean Enck <enckse@gmail.com>
2017-11-01 13:08:25 -0400

    working towards #12

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='survey',
+    version='__VERSION__',
+    long_description="survey that is fully deployable without internet",
+    packages=['survey'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask']
+)
