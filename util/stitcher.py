025bbb2567c619c2c591085696d06628af62c845
Sean Enck <enckse@gmail.com>
2017-11-01 17:26:20 -0400

    produce md and general cleanup

--- util/stitcher.py
+++ util/stitcher.py
@@ -29,28 +29,33 @@ def get_all(input_path, include):
                     idx = idx + 1
                 yield j
 
-def stitch(path, json_out, csv_out, include):
+def stitch(path, json_out, csv_out, md_out, include):
     """Stitch together."""
     json_out.write("[");
     first = True
-    keys = []
-    writer = csv.DictWriter(csv_out,
-                            fieldnames=keys,
-                            quoting=csv.QUOTE_NONNUMERIC)
-    writer.writeheader()
+    writer = None
     for obj in get_all(path, include):
-        writer.writerow(obj)
-        if not first:
+        keys = obj.keys()
+        if first:
+            writer = csv.DictWriter(csv_out,
+                                    fieldnames=keys,
+                                    quoting=csv.QUOTE_NONNUMERIC)
+            writer.writeheader()
+        else:
             json_out.write(',')
+        writer.writerow(obj)
         first = False
         json_out.write(json.dumps(obj,
                                   sort_keys=True,
                                   indent=4,
                                   separators=(',', ': ')
                                   ))
-        for k in obj.keys():
-            if k not in keys:
-                keys.append(k)
+        for k in keys:
+            md_out.write(" #### {}\n".format(k))
+            md_out.write("```")
+            md_out.write(obj[k])
+            md_out.write("```")
+            md_out.write("\n")
     json_out.write("]")
 
 def main():
@@ -66,12 +71,15 @@ def main():
                         help="include files")
     parser.add_argument("--output", required=True, help="output file name")
     args = parser.parse_args()
+    output_name = args.output
     with open(output_name + '.json', 'w') as json_file:
         with open(output_name + '.csv', 'w') as csv_file:
-            stitch(args.store,
-                   args.output + ".json",
-                   args.output + ".csv",
-                   args.include)
+            with open(output_name + ".md", 'w') as md_file:
+                stitch(args.store,
+                       json_file,
+                       csv_file,
+                       md_file,
+                       args.include)
 
 if __name__ == "__main__":
     main()
