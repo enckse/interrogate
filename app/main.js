5d5d22c25ed828c943866490789a573a099184a7
Sean Enck <enckse@gmail.com>
2018-01-29 20:25:45 -0500

    this begins to work

--- app/main.js
+++ app/main.js
@@ -1,21 +1,8 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-const {session} = require('electron')
-const path = require('path')
-const url = require('url')
-
 let mainWindow
-
 function createWindow () {
-  const filter = {
-      urls: ['http://localhost:8080/']
-  }
-  session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
-      console.log(details)
-      callback({cancel: false})
-  })
-
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.webContents.openDevTools()
   mainWindow.loadURL('http://localhost:8080')
