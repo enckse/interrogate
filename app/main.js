12e8969b4b885912e0481c8bd5cf9d39aa6f2a6e
Sean Enck <enckse@gmail.com>
2018-01-30 14:41:05 -0500

    cleaning up and allowing fallback

--- app/main.js
+++ app/main.js
@@ -1,11 +1,20 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const path = require('path')
+const fs = require('fs')
 let mainWindow
 function createWindow () {
+  let config = path.join(app.getPath('userData'), 'survey.url')
+  let url = undefined
+  if (fs.existsSync(config)) {
+      url = fs.readFileSync(config)
+  }
+  if (!url || url.length === 0 || url === undefined) {
+    url = "http://localhost:8080"
+  }
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.webContents.openDevTools()
-  mainWindow.loadURL('http://localhost:8080')
+  mainWindow.loadURL(url)
   mainWindow.on('closed', function () {
     mainWindow = null
   })
