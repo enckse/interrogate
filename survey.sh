67d3861a5a0dd19262a64c48c819db539b58def3
Sean Enck <enckse@gmail.com>
2018-02-01 09:11:05 -0500

    no longer need to keep alive on self

--- survey.sh
+++ survey.sh
@@ -7,9 +7,5 @@ args="$@"
 if [ -z "$args" ]; then
     args=$SURVEY_SETTINGS
 fi
-exit_code=10
-while [ $exit_code -eq 10 ]; do
-    echo "loading survey $args"
-    survey $args
-    exit_code=$?
-done
+echo "loading survey $args"
+survey $args
