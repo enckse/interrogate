e37cff557ce1ef12337f7cb81e8f5627a4160561
Sean Enck <enckse@gmail.com>
2018-02-18 20:10:07 -0500

    beginning work

--- survey/survey.go
+++ survey/survey.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+    "net/http"
+    "html/template"
+    "path/filepath"
+    "log"
+)
+
+const startTemplate = "begin.html"
+
+func readTemplate(directory string, tmpl string) *template.Template {
+    file := filepath.Join(directory, tmpl)
+    t, err := template.ParseFiles(file)
+    if err != nil {
+        log.Print("Unable to read template: " + file)
+        log.Print(err)
+        panic("bad template")
+    }
+    return t
+}
+
 func main() {
+    http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+    })
 }
