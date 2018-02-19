453d040db5a58a137a48a4bdcd8caab5ffd7fef8
Sean Enck <enckse@gmail.com>
2018-02-18 20:10:28 -0500

    spacing

--- survey/survey.go
+++ survey/survey.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "net/http"
-    "html/template"
-    "path/filepath"
-    "log"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 const startTemplate = "begin.html"
 
 func readTemplate(directory string, tmpl string) *template.Template {
-    file := filepath.Join(directory, tmpl)
-    t, err := template.ParseFiles(file)
-    if err != nil {
-        log.Print("Unable to read template: " + file)
-        log.Print(err)
-        panic("bad template")
-    }
-    return t
+	file := filepath.Join(directory, tmpl)
+	t, err := template.ParseFiles(file)
+	if err != nil {
+		log.Print("Unable to read template: " + file)
+		log.Print(err)
+		panic("bad template")
+	}
+	return t
 }
 
 func main() {
-    http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
-    })
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+	})
 }
