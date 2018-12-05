5d163cdecbdf710fc7f43577138fb8e60d9a2685
Sean Enck <enckse@voidedtech.com>
2018-12-05 08:46:44 -0500

    multiple fixes

--- supporting/stitcher.py
+++ supporting/stitcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 """Stitches survey results together."""
+import os
 import json
 import argparse
 import csv
@@ -46,7 +47,7 @@ class Result(object):
     def to_md(self, md_file):
         """Convert to md file."""
         md_file.write("---")
-        md_file.write("### {} ({})".format(client, mode))
+        md_file.write("### {} ({})".format(self.client, self.mode))
         for d in self.data:
             d.to_md(md_file)
 
@@ -95,7 +96,7 @@ def run(args):
         f = files[idx]
         idx += 1
         obj = {"client": client, "mode": m}
-        with open(os.path.join(args.dir, f)) as f:
+        with open(os.path.join(args.dir, f + ".json")) as f:
             obj["data"] = json.loads(f.read())
         objs.append(obj)
     for mani in objs:
@@ -103,30 +104,30 @@ def run(args):
         mode = mani["mode"]
         data = mani['data']
         idx = {}
-        for v in o:
-            values = o[v]
+        for v in data:
+            values = data[v]
             if v in ["session", "client"]:
                 continue
             idx[int(v)] = values
         datum = []
         for k in sorted(idx.keys()):
-            text = q[k][0]
-            typed = q[k][1]
+            text = questions[k][0]
+            typed = questions[k][1]
             datum.append(Data(text, typed, idx[k]))
         obj = Result(mode, client, datum)
         results.append(obj)
-    fields = list([x[0] for x in questions])
+    fields = list([x[0] for x in questions]) + [_CLIENT, _MODE]
     markdown_file = args.out + ".md"
-    with open(args.out + ".json") as j_file:
-        with open(markdown_file) as md_file:
-            with open(args.out + ".csv") as c_file:
+    with open(args.out + ".json", 'w') as j_file:
+        with open(markdown_file, 'w') as md_file:
+            with open(args.out + ".csv", 'w') as c_file:
                 csv_file = csv.DictWriter(c_file, fieldnames=fields)
                 csv_file.writeheader()
                 for result in results:
-                    result.to_json(j_file)
                     result.to_md(md_file)
                     result.to_csv(csv_file)
-    with open(args.out + ".html") as h_file:
+                j_file.write(json.dumps(results))
+    with open(args.out + ".html", 'w') as h_file:
         with open(markdown_file) as m_file:
             html = markdown.markdown(m_file.read())
             h_file.write(html)
