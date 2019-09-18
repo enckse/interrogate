d13e3049d9af8179e4de265243f0ad60ec2a4a26
Sean Enck <enckse@voidedtech.com>
2019-09-18 10:53:13 -0400

    stitcher tests

---
+++ test/expect/results.md
@@ -0,0 +1,68 @@
+
+---
+### 127.0.0.1
+
+```
+snapshot - testid - 2019-09-18T10-42-48
+```
+
+#### 0. What is this? (input)
+
+```
+ojioj
+```
+
+#### 1. Hidden (hidden)
+
+```
+<no response>
+```
+
+#### 2. Describe yourself (long)
+
+```
+ijoiojoj
+```
+
+#### 3. Your understanding (option)
+
+```
+High
+```
+
+#### 4. Show some label text (label)
+
+```
+<no response>
+```
+
+#### 6. Can you check this box? (checkbox)
+
+```
+on
+```
+
+#### 7. Pick a number, any number... (number)
+
+```
+<no response>
+```
+
+#### 8. Preference on sliders (slide)
+
+```
+20.00
+```
+
+#### 9. Can you check this box conditionally? (conditional)
+
+```
+0
+```
+
+#### 10. Is this long? (long)
+
+```
+ijojiojoijojioi
+```
+
--- test/run.sh
+++ test/run.sh
@@ -11,8 +11,11 @@ _run() {
     ../bin/survey --config settings.$1.conf &
     curl -s http://localhost:8080/survey/testid > bin/survey.$1.html
     curl -s http://localhost:8080/admin?token=123456 > bin/admin.$1.html
+    curl -s http://localhost:8080/snapshot/ -X POST -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' --data 'session=testid&1=&0=ojioj&2=ijoiojoj&3=High&4=&6=on&7=&8=20.00&9=0&10=ijojiojoijojioi'
     for f in admin survey; do
-        diff -u $f.$1.html bin/$f.$1.html
+        file=bin/$f.$1.html
+        sed -i "s#<td>test\_.*#<td>uid</td>#" $file
+        diff -u expect/$f.$1.html $file
         if [ $? -ne 0 ]; then
             failed=1
         fi
@@ -24,4 +27,16 @@ _run() {
 for f in $(ls *.yaml); do
     _run $(echo $f | sed "s/\.yaml//g")
 done
+../bin/survey-stitcher --manifest stitch/test.index.manifest --dir stitch/ --config stitch/run.config.test --out $PWD/bin/results
+for f in $(ls expect/results*); do
+    diff -u $f bin/$(basename $f)
+    if [ $? -ne 0 ]; then
+        failed=1
+    fi
+done
+test -s bin/results.tar.gz
+if [ $? -ne 0 ]; then
+    echo "invalid tar"
+    failed=1
+fi
 exit $failed
